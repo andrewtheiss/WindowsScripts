f671b13cda7ee85d356b2093ed548dc2e40d5b83
Andrew Theiss <andrew.theiss@gmail.com>
2022-10-05 11:22:55 -0700

    Update shortcuts.ps1

--- shortcuts.ps1
+++ shortcuts.ps1
@@ -1,17 +1,19 @@
-:: echo $profile to get profile location
+# `echo $profile` to get profile location on your system
 
-:: List all the possible aliases
+# List all the possible aliases
 function qq {
   Write-Output "Available Aliases"
   Write-Output "  htopics     => Honors Topics"
   Write-Output "  ecoin       => Ethereum Coin Stuff"
 }
 
+# Honors Topics we want to move to the location and see current version of the website
 function htopics {
   Set-Location -Path "D:\Program Files\Github\AdvancedTopicsCS"
   firebase-serve
 }
 
+# Ethereum coin we want to start remixd in the correct directory
 function ecoin {
   Set-Location -Path "D:\Program Files\Github\EthereumCoin"
   remixd
