42e41efb11f6d1b55b8d202b8983efa39d76d23d
Andrew Theiss <andrew.theiss@gmail.com>
2022-11-17 18:08:03 -0800

    Update and rename cmdShortcuts.sh to cmdShortcuts.bat

---
+++ cmdShortcuts.bat
@@ -0,0 +1,3 @@
+:: In CMD target after cmd.exe /k "C:\Users\andre\Desktop\cmdShortcuts.bat"
+DOSKEY vype=cd /d "D:Program Files/Github/vyper-server"
+cls
--- cmdShortcuts.sh
+++
@@ -1,1 +0,0 @@
-DOSKEY vype=cd /d "D:Program Files/Github/vyper-server"
